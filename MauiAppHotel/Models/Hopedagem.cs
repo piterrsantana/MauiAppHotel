e11ec8de7e29532af33b124a17ab3035f9a5502c
Peterson Sant'Ana <piter_santana@hotmail.com>
2026-05-20 02:50:07 -0300

    Entrega ao professor

--- MauiAppHotel/Models/Hopedagem.cs
+++ MauiAppHotel/Models/Hopedagem.cs
@@ -4,6 +4,7 @@ namespace MauiAppHotel.Models
 {
     public class Hospedagem
     {
+        //Aqui definimos as variáveis para calcular o valor da hospedagem e entregar na tela final.
         public Quarto QuartoSelecionado { get; set; }
         public int QuantidadeAdultos { get; set; }
         public int QuantidadeCriancas { get; set; }
@@ -15,22 +16,22 @@ namespace MauiAppHotel.Models
         {
             get
             {
-                TimeSpan diferenca = DataCheckOut - DataCheckIn;
-                return diferenca.Days;
+                TimeSpan diferenca = DataCheckOut - DataCheckIn; //Aqui calculamos quantos dias de estadia. Dia de saída menos o dia de entrada.
+                return diferenca.Days; //Dias de estadia
             }
         }
 
-        // Propriedade calculada que realiza a conta matemática total da hospedagem
+        // Propriedade calculada que realiza a conta matemática total da hospedagem.
         public double ValorTotal
         {
             get
             {
-                if (QuartoSelecionado == null) return 0;
+                if (QuartoSelecionado == null) return 0; //Se não for selecionado nenhum quarto o valor exibido será 0.
 
-                double valorAdultos = QuantidadeAdultos * QuartoSelecionado.ValorDiariaAdulto;
-                double valorCriancas = QuantidadeCriancas * QuartoSelecionado.ValorDiariaCrianca;
+                double valorAdultos = QuantidadeAdultos * QuartoSelecionado.ValorDiariaAdulto; //Calculo de qual valor para o total de Adultos no quarto selecionado.
+                double valorCriancas = QuantidadeCriancas * QuartoSelecionado.ValorDiariaCrianca; //Calculo de qual valor para o total de Crianças no quarto selecionado.
 
-                return (valorAdultos + valorCriancas) * QuantidadeDias;
+                return (valorAdultos + valorCriancas) * QuantidadeDias; // Resultado final =  soma do preço para os adultos e ciranças multiplicado pelo nº de dias de estadia.
             }
         }
     }
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -8,6 +8,7 @@ public partial class ContratacaoHospedagem : ContentPage
 {
     App PropriedadesdoApp;
 
+    // Construtor da Tela (Executa ao abrir a página)
     public ContratacaoHospedagem()
     {
         InitializeComponent();
@@ -15,23 +16,23 @@ public partial class ContratacaoHospedagem : ContentPage
         pck_quarto.ItemsSource = PropriedadesdoApp.lista_quartos;
 
         // Define as regras iniciais de data
-        dtpck_checkin.MinimumDate = DateTime.Now;
-        dtpck_checkout.MinimumDate = DateTime.Now.AddDays(1);
-        dtpck_checkout.MaximumDate = DateTime.Now.AddMonths(6);
+        dtpck_checkin.MinimumDate = DateTime.Now; //Impede escolha de datas no passado.
+        dtpck_checkout.MinimumDate = DateTime.Now.AddDays(1);//Exige pelo menos um dia de estadia. Saida somente um dia depois de Hoje
+        dtpck_checkout.MaximumDate = DateTime.Now.AddMonths(6); //Limite para escolha de data. Maximo 6 meses o que protege preços desatualizados
     }
 
-    private async void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)//Ao clicar no Botão "Sobre", o App será direcionado para a página sobre.
     {
         await Navigation.PushAsync(new Sobre());
     }
 
     // Botão Avançar
-    private async void Button_Clicked_2(object sender, EventArgs e)
+    private async void Button_Clicked_2(object sender, EventArgs e) //Ao clicar no Botão "Sobre", o App será direcionado para a página sobre.
     {
-        try
+        try //Proteção para o App não trvar enquanto realiza processos. "Tente" isso, "Se der erro" faça isso.
         {
             // Validação: Verifica se o usuário escolheu um quarto antes de avançar
-            if (pck_quarto.SelectedItem == null)
+            if (pck_quarto.SelectedItem == null)//Se não escolher nenhum quarto Avise: ....
             {
                 await DisplayAlert("Ops", "Por favor, selecione uma acomodação.", "Ok");
                 return;
@@ -40,11 +41,11 @@ public partial class ContratacaoHospedagem : ContentPage
             // Monta o objeto de hospedagem com os dados da tela
             Hospedagem hospedagemContratada = new Hospedagem
             {
-                QuantidadeAdultos = Convert.ToInt32(stp_adultos.Value),
+                QuantidadeAdultos = Convert.ToInt32(stp_adultos.Value), //Resgata os valores escolhidos pelo o usuário, converte em nº e armazena
                 QuantidadeCriancas = Convert.ToInt32(stp_crianca.Value),
                 DataCheckIn = (DateTime)dtpck_checkin.Date,
                 DataCheckOut = (DateTime)dtpck_checkout.Date,
-                QuartoSelecionado = (Quarto)pck_quarto.SelectedItem
+                QuartoSelecionado = (Quarto)pck_quarto.SelectedItem //Guarda o tipo de quarto com os preços escolhidos pelo usuário
             };
 
             // Envia o objeto criado para o construtor da próxima página
@@ -57,7 +58,7 @@ public partial class ContratacaoHospedagem : ContentPage
     }
 
     // Atualiza dinamicamente os limites do checkout ao mudar o check-in
-    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)//Método para proteger o período determinado de escolha em 6 meses.
     {
         DatePicker elemento = sender as DatePicker;
         DateTime data_selecionada_checkin = (DateTime)elemento.Date;
